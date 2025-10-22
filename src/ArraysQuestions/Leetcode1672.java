5d1be4577319ade426d8c8d01f167e8c60b688d7
sanket <sanketpaygude195@gmail.com>
2025-10-22 21:06:26 +0530

    Leetcode Question no 1672

---
+++ src/ArraysQuestions/Leetcode1672.java
@@ -0,0 +1,35 @@
+package ArraysQuestions;
+
+//1672. Richest customer wealth
+//Input: accounts = [[1,2,3],[3,2,1]]
+//Output: 6
+//Explanation:
+//        1st customer has wealth = 1 + 2 + 3 = 6
+//2nd customer has wealth = 3 + 2 + 1 = 6
+//Both customers are considered the richest with a wealth of 6 each, so return 6.
+public class Leetcode1672 {
+
+    int max=0;
+    public static int maximumWealth(int[][] accounts)
+    {
+        int max=0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+             sum += accounts[i][j];
+            }
+
+            if(sum>max)
+            {
+                max=sum;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] account={{1, 2, 3},{3,2,1}};
+        System.out.println(maximumWealth(account));
+    }
+
+}
