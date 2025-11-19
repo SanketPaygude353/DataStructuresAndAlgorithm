b12b16e983a9b35c4791681fb82a278762b9242b
sanket <sanketpaygude195@gmail.com>
2025-11-19 19:17:48 +0530

    Leetcode Question no 1

---
+++ src/ArraysQuestions/Easy/LeetCode01.java
@@ -0,0 +1,32 @@
+package ArraysQuestions.Easy;
+//1. Two Sum
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+import java.util.Arrays;
+
+public class LeetCode01 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+
+}
